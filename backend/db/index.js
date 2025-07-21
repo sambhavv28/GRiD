98441cb0592f505775d8177588baac19274dab01
Sambhav Sethi <159689398+sambhavv28@users.noreply.github.com>
2025-07-21 17:42:13 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: `${process.env.DB_PASSWORD}`, // ← Force convert to string
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),      // ← Convert to number
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,  
+  },
 });
 
 module.exports = pool;
